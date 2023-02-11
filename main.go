29b210d37479dfd50f0b3debb1c88f498f6c3e5b
Rendra <rendra@investree.id>
2023-02-11 14:10:23 +0700

    [RENDRA] - Create config and database connection

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/rendramakmur/freefolks-fc/config"
+)
 
+func main() {
+	config.Read()
+	var _ = config.ConnectDB()
+	f := fiber.New()
+
+	f.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON("Freefolks FC service API")
+	})
+
+	if err := f.Listen(":8080"); err != nil {
+		panic(err)
+	}
 }
